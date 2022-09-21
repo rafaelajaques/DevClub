dbb04fd261a2904351a93dfa653f5f9dceff6dfd
rafaelajaques <rafaelajaques@yahoo.com.br>
2022-09-21 13:25:11 -0300

    Acrescentada aula de array

---
+++ JavaScript pt. 1/9.2 Array.js
@@ -0,0 +1,34 @@
+// Array = vetor, lista
+
+const numbers = [1, 5, 10, 15]
+
+console.log(numbers)
+console.log(numbers[1])
+
+// Nos arrays começamos pela posição zero (0)
+// Se no console.log pedir uma posção que não existe no array, o resultado será "undefined"
+
+const people = [{
+    name: "Rafaela",
+    age: 32,
+    sex: "female",
+    height: 1.73,
+    weight: 59
+},
+{
+    name: "Caroline",
+    age: 25,
+    sex: "female",
+    height: 1.67,
+    weight: 70
+},
+{
+    name: "Simone",
+    age: 54,
+    sex: "female",
+    height: 1.65,
+    weight: 67
+}
+]
+
+// Os arrays podem ser mesclados, contendo número, frase ou objeto no mesmo array, por exemplo.
